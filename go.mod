ad4cc80b29a3958f8a05c84b3a4012e34a9b906a
Brian G. Merrell <brian.merrell@walmartlabs.com>
2020-12-28 14:41:57 -0700

    Continued work on poc

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/bgmerrell/go-plugin-poc
 
 go 1.14
+
+require (
+	github.com/golang/protobuf v1.3.4
+	github.com/hashicorp/go-plugin v1.4.0
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	google.golang.org/grpc v1.27.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/bgmerrell/go-plugin-poc/shared"
+
+	"github.com/hashicorp/go-plugin"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	// We don't want to see the plugin logs.
+	log.SetOutput(os.Stderr)
+
+	kv_plugin_env := os.Getenv("KV_PLUGIN")
+	if kv_plugin_env == "" {
+		fmt.Println("No KV_PLUGIN env variable")
+		os.Exit(1)
+	}
+
+	// We're a host. Start by launching the plugin process.
+	client := plugin.NewClient(&plugin.ClientConfig{
+		HandshakeConfig: shared.Handshake,
+		Plugins:         shared.PluginMap,
+		Cmd:             exec.Command("sh", "-c", kv_plugin_env),
+		AllowedProtocols: []plugin.Protocol{
+			plugin.ProtocolNetRPC, plugin.ProtocolGRPC},
+	})
+	defer client.Kill()
+
+	// Connect via RPC
+	rpcClient, err := client.Client()
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
+
+	// Request the plugin
+	raw, err := rpcClient.Dispense("kv_grpc")
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
+
+	// We should have a KV store now! This feels like a normal interface
+	// implementation but is in fact over an RPC connection.
+	kv := raw.(shared.KV)
+	os.Args = os.Args[1:]
+	switch os.Args[0] {
+	case "get":
+		result, err := kv.Get(os.Args[1])
+		if err != nil {
+			fmt.Println("Error:", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println(string(result))
+
+	case "put":
+		err := kv.Put(os.Args[1], []byte(os.Args[2]))
+		if err != nil {
+			fmt.Println("Error:", err.Error())
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Printf("Please only use 'get' or 'put', given: %q", os.Args[0])
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
